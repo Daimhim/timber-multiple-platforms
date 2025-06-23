087a9d087d6df7a303ba59df74677bebdeaac3f5
Daimhim <daimhim@163.com>
2025-06-23 10:25:08 +0800

    修改打包错误 You can't map a property that does not exist: propertyName=archiveName

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -2,7 +2,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("multiplatform")
-    id("org.jetbrains.compose")
+    id("org.jetbrains.compose") // 修复了缺少的括号和引号
+//    id("maven-publish") // 正确应用Maven发布插件
 }
 
 group = "org.daimhim.timber"
@@ -11,8 +12,15 @@ version = "1.0-SNAPSHOT"
 
 kotlin {
     jvm {
-//        jvmToolchain(11)
         withJava()
+        // 将JAR配置移至jvm目标平台内
+//        tasks.getByName<Jar>("jar") {
+//            archiveFileName.set("${project.name}-${project.version}.jar")
+//        }
+        tasks.named<Jar>("jar") {
+            // 使用archiveFileName替代archiveName
+            archiveFileName.set("${project.name}-${project.version}.jar")
+        }
     }
     sourceSets {
         val jvmMain by getting {
@@ -35,3 +43,26 @@ compose.desktop {
         }
     }
 }
+// 配置Maven发布
+//publishing {
+//    publications {
+//        create<MavenPublication>("maven") {
+//            from(components["java"])
+//            // 使用archiveFileName替代已弃用的archiveName
+//            artifact(tasks["jar"]) {
+//                classifier = ""
+//                builtBy(tasks["jar"])
+//            }
+//        }
+//    }
+//}
+
+//kotlin {
+//    jvm {
+//        withJava()
+//        tasks.named<Jar>("jar") {
+//            // 使用archiveFileName替代archiveName
+//            archiveFileName.set("${project.name}-${project.version}.jar")
+//        }
+//    }
+//}
